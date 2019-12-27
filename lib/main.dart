ea84b9443371e3ba64fa093a27bb2c43f10cd1a6
Luan Nico <luanpotter27@gmail.com>
2019-12-27 12:31:46 -0500

    adding more buttons to reflect new functionality

--- lib/main.dart
+++ lib/main.dart
@@ -27,11 +27,13 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+final urls = [
+  'https://luan.xyz/files/audio/ambient_c_motion.mp3',
+  'https://luan.xyz/files/audio/nasa_on_a_mission.mp3',
+  'http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio1xtra_mf_p',
+];
 
-  void play() {
-    player.play('https://luan.xyz/files/audio/ambient_c_motion.mp3');
-  }
+class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
@@ -42,11 +44,40 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FlatButton(
+                  child: Text('Set Url 0'),
+                  onPressed: () => player.setUrl(urls[0]),
+                ),
+                FlatButton(
+                  child: Text('Set Url 1'),
+                  onPressed: () => player.setUrl(urls[1]),
+                ),
+                FlatButton(
+                  child: Text('Set Url 2'),
+                  onPressed: () => player.setUrl(urls[2]),
+                ),
+              ],
+            ),
             FlatButton(
               child: Text('Play'),
-              onPressed: () => this.play(),
-            )
+              onPressed: () => player.play(urls[0]),
+            ),
+            FlatButton(
+              child: Text('Resume'),
+              onPressed: () => player.resume(),
+            ),
+            FlatButton(
+              child: Text('Pause'),
+              onPressed: () => player.pause(),
+            ),
+            FlatButton(
+              child: Text('Stop'),
+              onPressed: () => player.stop(),
+            ),
           ],
         ),
       ),
